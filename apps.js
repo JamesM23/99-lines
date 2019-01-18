d3498d8f0b845e3ed1ce959dfdaa1e3da33f4754
JamesM23 <46689705+JamesM23@users.noreply.github.com>
2019-01-18 16:41:23 -0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,19 @@
+// let friend = document.querySelectorAll('.friends');
+let friends = ['john', 'kim', 'mike', 'joe', 'sam']
+
+for (i = 0; i < friends.length; i++) {
+    for (j = 99; j > 0; j--) {
+        if (j > 1) {
+            console.log(j + " lines of code in the file," + j + " lines of code;" + friends[i] + " strikes one out, clears it all out," + (j - 1) + " lines of code in the file,"
+            ) 
+        }    else {
+            console.log ("1 line of code in the file, 1 line of code;" + friends[i] + "strikes one out, clears it all out, no more lines of code in the file,"
+                )
+        }
+
+    }
+}
+
+
+
+
